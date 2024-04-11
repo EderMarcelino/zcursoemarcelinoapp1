d769fd6ddb954e1bffb3204368032d778380a944
zcursoemarcelinoapp1 <edermarcelino@hotmail.com>
2024-04-11 19:07:09 -0300

    primeiro commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("zcursoemarcelinoapp1.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zcursoemarcelinoapp1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
